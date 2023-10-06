4cbafbe4331c8cb3a874798cf8c194a9f9dc3776
pahuber <philhuber@live.com>
2023-10-06 18:34:53 +0200

    Add emma-x array and docstrings

--- lifesim2/observatory/array_configurations.py
+++ lifesim2/observatory/array_configurations.py
@@ -1,10 +1,15 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from enum import Enum
 
+import astropy.units
 import numpy as np
+
+from lifesim2.util.matrix import get_2d_rotation_matrix
 
 
 class ArrayConfigurationEnum(Enum):
+    """Enum representing the different array configuration types.
+    """
     EMMA_X_CIRCULAR_ROTATION = 'emma-x-circular-rotation'
     EMMA_X_DOUBLE_STRETCH = 'emma-x-double-stretch'
     EQUILATERAL_TRIANGLE_CIRCULAR_ROTATION = 'equilateral-triangle-circular-rotation'
@@ -12,22 +17,66 @@ class ArrayConfigurationEnum(Enum):
 
 
 class ArrayConfiguration(ABC):
-    def __init__(self, type: str):
+    """Class representation of a collector array configuration.
+    """
+
+    def __init__(self,
+                 baseline_minimum: astropy.units.Quantity,
+                 baseline_maximum: astropy.units.Quantity,
+                 baseline_ratio: astropy.units.Quantity,
+                 modulation_period: astropy.units.Quantity):
+        """Constructor method.
+        :param baseline_minimum: Minimum value of the baseline in meters
+        :param baseline_maximum: Maximum value of the baseline in meters
+        :param baseline_ratio: Ratio between the respective baselines
+        :param modulation_period: Period for one full array modulation (e.g. rotation period for circular rotation)
+        """
+
+        super().__init__()
+
+    @abstractmethod
+    def get_collector_positions(self, time: astropy.units.Quantity) -> np.ndarray:
+        """Return an array containing the time-dependent x- and y-coordinates of the collectors.
+        :param time: Time variable in seconds
+        :return: An array containing the coordinates.
+        """
         pass
 
 
 class EmmaXCircularRotation(ArrayConfiguration):
-    def __init__(self):
-        pass
+    """Class representation of the Emma-X array configuration with circular rotation of the array.
+    """
+
+    def get_collector_positions(self, time: astropy.units.Quantity) -> np.ndarray:
+        period = 100
+        rotation_matrix = get_2d_rotation_matrix(time, period)
+        baseline = 10
+        baseline_ratio = 6
+        emma_x_static = baseline / 2 * np.array([[baseline_ratio, -baseline_ratio, -baseline_ratio, baseline_ratio],
+                                                 [1, 1, -1, -1]])
+
+        return np.matmul(rotation_matrix, emma_x_static)
+
 
 class EmmaXDoubleStretch(ArrayConfiguration):
-    def __init__(self):
+    """Class representation of the Emma-X array configuration with double stretching of the array.
+    """
+
+    def get_collector_positions(self, time: float) -> np.ndarray:
         pass
+
 
 class EquilateralTriangleCircularRotation(ArrayConfiguration):
-    def __init__(self):
+    """Class representation of an equilateral triangle configuration with circular rotation of the array.
+    """
+
+    def get_collector_positions(self, time: float) -> np.ndarray:
         pass
 
+
 class RegularPentagonCircularRotation(ArrayConfiguration):
-    def __init__(self):
+    """Class representation of a regular pentagon configuration with circular rotation of the array.
+    """
+
+    def get_collector_positions(self, time: float) -> np.ndarray:
         pass
