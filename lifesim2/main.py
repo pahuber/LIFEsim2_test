7c3e86de63e8e72d607256a738f554ddc9ea53d8
pahuber <philhuber@live.com>
2023-10-10 16:35:15 +0200

    Add main simulation method

--- lifesim2/main.py
+++ lifesim2/main.py
@@ -1,10 +1,10 @@
-from lifesim2.observatory.beam_combination_schemes import DoubleBracewell4
-from lifesim2.observatory.instrument_specification import InstrumentSpecification
-from lifesim2.observatory.observatory import Observatory
+from lifesim2.observation.observation import Observation
 
+# Specify paths
+path_to_config_file = r'C:\Users\huber\Desktop\LIFEsim2\config.yaml'
 
-observatory = Observatory()
-observatory.load_from_config(r'C:\Users\huber\Desktop\LIFEsim2\observatory.yaml')
+# Initialize observation
+observation = Observation(path_to_config_file=path_to_config_file)
 
-print(observatory.array_configuration)
-print(observatory.beam_combination_scheme)
+# Run simulation of observation
+observation.run()
