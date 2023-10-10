935b09e6f46948af57de900381a2169f1bfebd80
pahuber <philhuber@live.com>
2023-10-10 16:33:33 +0200

    Update config reader

--- lifesim2/util/config_reader.py
+++ lifesim2/util/config_reader.py
@@ -13,7 +13,7 @@ class ConfigReader():
         self.path_to_config_file = path_to_config_file
         self._config_dict = dict()
         self._read_raw_config_file()
-        self._parse_instrument_specification_units()
+        self._parse_units()
 
     def _read_raw_config_file(self):
         """Read the configuration file and save its content in a dictionary.
@@ -21,12 +21,18 @@ class ConfigReader():
         with open(self.path_to_config_file, 'r') as config_file:
             self._config_dict = yaml.load(config_file, Loader=yaml.SafeLoader)
 
-    def _parse_instrument_specification_units(self):
-        """Parse the units of the instrument specification parameters and convert them to astropy quantities.
+    def _parse_units(self):
+        """Parse the units of the numerical parameters and convert them to astropy quantities.
         """
-        for key in self._config_dict['instrument_specification']:
-            self._config_dict['instrument_specification'][key] = \
-                u.Quantity(self._config_dict['instrument_specification'][key])
+        for key in self._config_dict['simulation'].keys():
+            self._config_dict['simulation'][key] = u.Quantity(self._config_dict['simulation'][key])
+
+        for key in self._config_dict['observation'].keys():
+            self._config_dict['observation'][key] = u.Quantity(self._config_dict['observation'][key])
+
+        for key in self._config_dict['observatory']['instrument_parameters']:
+            self._config_dict['observatory']['instrument_parameters'][key] = \
+                u.Quantity(self._config_dict['observatory']['instrument_parameters'][key])
 
     def get_config_from_file(self) -> dict():
         """Return a dictionary containing the content of the configuration file.
