e2280a202e7ecf5df30a7346052b574107dda1a4
pahuber <philhuber@live.com>
2023-10-11 12:48:10 +0200

    Fix naming

--- lifesim2/observatory/instrument_parameters.py
+++ lifesim2/observatory/instrument_parameters.py
@@ -2,5 +2,8 @@ from astropy import units as u
 
 
 class InstrumentParameters():
-    def __init__(self, specification_dict: dict() = None):
+    def __init__(self, parameters_dict: dict() = None):
         self.aperture_radius = 4 * u.m / 2
+        self._config_dict = None
+
+    # def set_from_config(self):
