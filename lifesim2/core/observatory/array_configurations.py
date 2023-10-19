Fix units

lifesim2/core/observatory/array_configurations.py
@@ -116,16 +116,15 @@ class EquilateralTriangleCircularRotation(ArrayConfiguration):
     """
 
     def get_collector_positions(self, time: float) -> np.ndarray:
-        units = self.baseline.unit
         height = np.sqrt(3) / 2 * self.baseline
         height_to_center = height / 3
         rotation_matrix = get_2d_rotation_matrix(time, self.modulation_period)
 
         equilateral_triangle_static = np.array(
             [[0, self.baseline.value / 2, -self.baseline.value / 2],
-             [height.value - height_to_center.value, -height_to_center.value, -height_to_center.value]]) * units
+             [height.value - height_to_center.value, -height_to_center.value, -height_to_center.value]])
 
-        return np.matmul(rotation_matrix, equilateral_triangle_static)
+        return np.matmul(rotation_matrix, equilateral_triangle_static) * self.baseline.unit
 
     def get_optimal_baseline(self, wavelength: astropy.units.Quantity,
                              optimal_angular_distance: astropy.units.Quantity):
@@ -143,7 +142,7 @@ class RegularPentagonCircularRotation(ArrayConfiguration):
         :param angle: The angle at which the collector is located
         :return: The x position
         """
-        return 0.851 * self.baseline * np.cos(angle)
+        return 0.851 * self.baseline.value * np.cos(angle)
 
     def _y(self, angle) -> astropy.units.Quantity:
         """Return the y position.
@@ -151,15 +150,15 @@ class RegularPentagonCircularRotation(ArrayConfiguration):
         :param angle: The angle at which the collector is located
         :return: The y position
         """
-        return 0.851 * self.baseline * np.sin(angle)
+        return 0.851 * self.baseline.value * np.sin(angle)
 
     def get_collector_positions(self, time: float) -> np.ndarray:
         angles = [0, 2 * np.pi / 5, 4 * np.pi / 5, 6 * np.pi / 5, 8 * np.pi / 5]
         rotation_matrix = get_2d_rotation_matrix(time, self.modulation_period)
-        pentagon_static = [
+        pentagon_static = np.array([
             [self._x(angles[0]), self._x(angles[1]), self._x(angles[2]), self._x(angles[3]), self._x(angles[4])],
-            [self._y(angles[0]), self._y(angles[1]), self._y(angles[2]), self._y(angles[3]), self._y(angles[4])]]
-        return np.matmul(rotation_matrix, pentagon_static)
+            [self._y(angles[0]), self._y(angles[1]), self._y(angles[2]), self._y(angles[3]), self._y(angles[4])]])
+        return np.matmul(rotation_matrix, pentagon_static) * self.baseline.unit
 
     def get_optimal_baseline(self, wavelength: astropy.units.Quantity,
                              optimal_angular_distance: astropy.units.Quantity):

lifesim2/core/observatory/beam_combination_schemes.py
@@ -91,9 +91,9 @@ class Kernel5(BeamCombinationScheme):
     """Class representation of a Kernel nulling beam combination scheme.
     """
 
-    def exp(self, number: int) -> float:
+    def _exp(self, number: int) -> float:
         """Return the exponent.
-        
+
         :param number: The number in the numerator
         :return: The exponent
         """
@@ -101,10 +101,10 @@ class Kernel5(BeamCombinationScheme):
 
     def get_beam_combination_transfer_matrix(self) -> np.ndarray:
         return 1 / np.sqrt(5) * np.array([[1, 1, 1, 1, 1],
-                                          [1, self.exp(2), self.exp(4), self.exp(6), self.exp(8)],
-                                          [1, self.exp(4), self.exp(8), self.exp(2), self.exp(6)],
-                                          [1, self.exp(6), self.exp(2), self.exp(8), self.exp(4)],
-                                          [1, self.exp(8), self.exp(6), self.exp(4), self.exp(2)]])
+                                          [1, self._exp(2), self._exp(4), self._exp(6), self._exp(8)],
+                                          [1, self._exp(4), self._exp(8), self._exp(2), self._exp(6)],
+                                          [1, self._exp(6), self._exp(2), self._exp(8), self._exp(4)],
+                                          [1, self._exp(8), self._exp(6), self._exp(4), self._exp(2)]])
 
     def get_differential_intensity_response_indices(self) -> list:
         return [(1, 4), (2, 3)]