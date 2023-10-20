301675dfa3d5f8b79d8c6516cfe444d7f2d4acd2
pahuber <philhuber@live.com>
2023-10-20 13:09:30 +0200

    Reorder methods

--- lifesim2/util/animation.py
+++ lifesim2/util/animation.py
@@ -42,6 +42,101 @@ class Animator():
         self.photon_rate_list = []
         self.time_index_list = []
 
+    def _get_overlay_cmap(self) -> matplotlib.colors.LinearSegmentedColormap:
+        """Create a color map to overlay the source on the differential intensity response plot.
+
+        :return: The colormap
+        """
+        colors = [(0, 1, 1, c) for c in np.linspace(0, 1, 100)]
+        return mcolors.LinearSegmentedColormap.from_list('mycmap', colors, N=2)
+
+    def _prepare_collector_position_image(self,
+                                          gs: matplotlib.gridspec.GridSpec,
+                                          grid_size: int) -> matplotlib.image.AxesImage:
+        """Prepare the plot for the collector positions.
+
+        :param gs: The GridSpec object
+        :param grid_size: The grid size
+        :return: The image
+        """
+        ax3 = self.figure.add_subplot(gs[0, 0])
+        image_collector_positions, = ax3.plot([], [], 'bo')
+
+        extent = 50
+        labels = np.linspace(-extent, extent, 2 * extent // 10 + 1)
+        ticks = np.linspace(0, grid_size, 2 * extent // 10 + 1)
+
+        ax3.set_title(f'Collector Positions', fontsize=10)
+        ax3.set_xlabel(f'Position (m)', fontsize=8)
+        ax3.set_ylabel(f'Position (m)', fontsize=8)
+        ax3.set_xlim(-extent, extent)
+        ax3.set_ylim(-extent, extent)
+        ax3.set_box_aspect(1)
+        ax3.set_aspect('equal')
+        ax3.tick_params(axis='both', which='major', labelsize=8)
+
+        return image_collector_positions
+
+    def _prepare_differential_intensity_response_plot(self,
+                                                      gs: matplotlib.gridspec.GridSpec,
+                                                      observation: Observation,
+                                                      grid_size: int,
+                                                      cmap: matplotlib.colors.LinearSegmentedColormap) -> matplotlib.image.AxesImage:
+        """Prepare the plot for the differential intensity response.
+
+        :param gs: The GridSpec object
+        :param observation: The observation object
+        :param grid_size: Tge grid size
+        :param cmap: The color map
+        :return: The image
+        """
+        ax1 = self.figure.add_subplot(gs[0, 1])
+        image_intensity_response = ax1.imshow(np.zeros((grid_size, grid_size)),
+                                              vmin=self.image_vmin,
+                                              vmax=self.image_vmax,
+                                              cmap='seismic')
+        ax1.imshow(observation.sources['Earth'].shape_map, cmap=cmap)
+
+        max = int(np.max(observation.sources[self.source_name].sky_coordinate_maps[0][0, :]).value * 1000)
+        labels = np.linspace(-max, max, grid_size // 10 + 1)
+        ticks = np.linspace(0, grid_size, grid_size // 10 + 1)
+
+        cb = self.figure.colorbar(image_intensity_response)
+        ax1.set_title(f'Differential Intensity Response', fontsize=10)
+        ax1.set_xlabel(f'Sky Coordinates (mas)', fontsize=8)
+        ax1.set_ylabel(f'Sky Coordinates (mas)', fontsize=8)
+        ax1.set_xticks(ticks=ticks, labels=labels)
+        ax1.set_yticks(ticks=ticks, labels=labels)
+        ax1.tick_params(axis='both', which='major', labelsize=8)
+        cb.ax.tick_params(labelsize=8)
+
+        return image_intensity_response
+
+    def _prepare_photon_rates_plot(self, gs: matplotlib.gridspec.GridSpec,
+                                   time_range: np.ndarray) -> matplotlib.image.AxesImage:
+        """Prepare the plot for the photon rates.
+
+        :param gs: The GirdSpec object
+        :param time_range: The time range
+        :return: The image
+        """
+        ax2 = self.figure.add_subplot(gs[1, :])
+        image_photon_rates, = ax2.plot([], [], 'b-o')
+
+        labels = time_range.to(u.h)
+        labels = [int(x.value) for x in labels]
+
+        ax2.set_title(f'Photon Rate', fontsize=10)
+        ax2.set_xlabel(f'Time (h)', fontsize=8)
+        ax2.set_ylabel(f'Photon Rate (a.u.)', fontsize=8)
+        ax2.set_xlim(0, len(time_range))
+        ax2.set_ylim(-0.1, 0.1)
+        ax2.set_xticklabels(labels=labels)
+        ax2.tick_params(axis='both', which='major', labelsize=8)
+        ax2.set_box_aspect(0.4)
+
+        return image_photon_rates
+
     def prepare_animation_writer(self, observation: Observation, time_range: np.ndarray, grid_size: int):
         """Prepare the animation writer.
 
@@ -97,98 +192,3 @@ class Animator():
             output.photon_rate_time_series[self.source_name][self.closest_wavelength][
                 self.differential_intensity_response_index][index_time])
         self.images[2].set_data(self.time_index_list, self.photon_rate_list)
-
-    def _get_overlay_cmap(self) -> matplotlib.colors.LinearSegmentedColormap:
-        """Create a color map to overlay the source on the differential intensity response plot.
-
-        :return: The colormap
-        """
-        colors = [(0, 1, 1, c) for c in np.linspace(0, 1, 100)]
-        return mcolors.LinearSegmentedColormap.from_list('mycmap', colors, N=2)
-
-    def _prepare_differential_intensity_response_plot(self,
-                                                      gs: matplotlib.gridspec.GridSpec,
-                                                      observation: Observation,
-                                                      grid_size: int,
-                                                      cmap: matplotlib.colors.LinearSegmentedColormap) -> matplotlib.image.AxesImage:
-        """Prepare the plot for the differential intensity response.
-
-        :param gs: The GridSpec object
-        :param observation: The observation object
-        :param grid_size: Tge grid size
-        :param cmap: The color map
-        :return: The image
-        """
-        ax1 = self.figure.add_subplot(gs[0, 1])
-        image_intensity_response = ax1.imshow(np.zeros((grid_size, grid_size)),
-                                              vmin=self.image_vmin,
-                                              vmax=self.image_vmax,
-                                              cmap='seismic')
-        ax1.imshow(observation.sources['Earth'].shape_map, cmap=cmap)
-
-        max = int(np.max(observation.sources[self.source_name].sky_coordinate_maps[0][0, :]).value * 1000)
-        labels = np.linspace(-max, max, grid_size // 10 + 1)
-        ticks = np.linspace(0, grid_size, grid_size // 10 + 1)
-
-        cb = self.figure.colorbar(image_intensity_response)
-        ax1.set_title(f'Differential Intensity Response', fontsize=10)
-        ax1.set_xlabel(f'Sky Coordinates (mas)', fontsize=8)
-        ax1.set_ylabel(f'Sky Coordinates (mas)', fontsize=8)
-        ax1.set_xticks(ticks=ticks, labels=labels)
-        ax1.set_yticks(ticks=ticks, labels=labels)
-        ax1.tick_params(axis='both', which='major', labelsize=8)
-        cb.ax.tick_params(labelsize=8)
-
-        return image_intensity_response
-
-    def _prepare_collector_position_image(self,
-                                          gs: matplotlib.gridspec.GridSpec,
-                                          grid_size: int) -> matplotlib.image.AxesImage:
-        """Prepare the plot for the collector positions.
-
-        :param gs: The GridSpec object
-        :param grid_size: The grid size
-        :return: The image
-        """
-        ax3 = self.figure.add_subplot(gs[0, 0])
-        image_collector_positions, = ax3.plot([], [], 'bo')
-
-        extent = 50
-        labels = np.linspace(-extent, extent, 2 * extent // 10 + 1)
-        ticks = np.linspace(0, grid_size, 2 * extent // 10 + 1)
-
-        ax3.set_title(f'Collector Positions', fontsize=10)
-        ax3.set_xlabel(f'Position (m)', fontsize=8)
-        ax3.set_ylabel(f'Position (m)', fontsize=8)
-        ax3.set_xlim(-extent, extent)
-        ax3.set_ylim(-extent, extent)
-        ax3.set_box_aspect(1)
-        ax3.set_aspect('equal')
-        ax3.tick_params(axis='both', which='major', labelsize=8)
-
-        return image_collector_positions
-
-    def _prepare_photon_rates_plot(self, gs: matplotlib.gridspec.GridSpec,
-                                   time_range: np.ndarray) -> matplotlib.image.AxesImage:
-        """Prepare the plot for the photon rates.
-
-        :param gs: The GirdSpec object
-        :param time_range: The time range
-        :return: The image
-        """
-        ax2 = self.figure.add_subplot(gs[1, :])
-        image_photon_rates, = ax2.plot([], [], 'b-o')
-
-        labels = time_range.to(u.h)
-        labels = [int(x.value) for x in labels]
-
-        ax2.set_title(f'Photon Rate', fontsize=10)
-        ax2.set_xlabel(f'Time (h)', fontsize=8)
-        ax2.set_ylabel(f'Photon Rate (a.u.)', fontsize=8)
-        ax2.set_xlim(0, len(time_range))
-        ax2.set_ylim(-0.1, 0.1)
-        ax2.set_xticklabels(labels=labels)
-        ax2.tick_params(axis='both', which='major', labelsize=8)
-        ax2.set_box_aspect(0.4)
-
-        return image_photon_rates
