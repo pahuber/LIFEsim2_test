Fix intensity response and photon rate units

lifesim2/core/intensity_response.py
@@ -20,11 +20,11 @@ def get_differential_intensity_responses(time,
     :param grid_size: The grid size
     :return: An array containing the differential intensity responses
     """
-    # TODO: fix units
     intensity_response_vector = get_intensity_responses(time, wavelength, observatory, source_sky_coordinate_maps,
                                                         grid_size)
     differential_indices = observatory.beam_combination_scheme.get_differential_intensity_response_indices()
-    differential_intensity_responses = np.zeros((len(differential_indices), grid_size, grid_size))
+    differential_intensity_responses = np.zeros(
+        (len(differential_indices), grid_size, grid_size)) * intensity_response_vector.unit
     for index_index, index_pair in enumerate(differential_indices):
         differential_intensity_responses[index_index] = intensity_response_vector[index_pair[0]] - \
                                                         intensity_response_vector[
@@ -57,12 +57,12 @@ def get_intensity_responses(time: astropy.units.Quantity,
 
     beam_combination_matrix = observatory.beam_combination_scheme.get_beam_combination_transfer_matrix()
 
-    intensity_response_unperturbed_vector = np.reshape(abs(
-        np.dot(beam_combination_matrix, input_complex_amplitude_unperturbed_vector)) ** 2,
-                                                       (
-                                                           observatory.beam_combination_scheme.number_of_outputs,
-                                                           grid_size,
-                                                           grid_size))
+    # intensity_response_unperturbed_vector = np.reshape(abs(
+    #     np.dot(beam_combination_matrix, input_complex_amplitude_unperturbed_vector)) ** 2,
+    #                                                    (
+    #                                                        observatory.beam_combination_scheme.number_of_outputs,
+    #                                                        grid_size,
+    #                                                        grid_size))
 
     intensity_response_perturbed_vector = np.reshape(abs(
         np.dot(beam_combination_matrix, input_complex_amplitude_perturbed_vector)) ** 2,
@@ -72,8 +72,8 @@ def get_intensity_responses(time: astropy.units.Quantity,
                                                          grid_size))
 
     # Normalize perturbed intensity response vector
-    for index in range(len(intensity_response_perturbed_vector)):
-        intensity_response_perturbed_vector[index] /= np.max(intensity_response_unperturbed_vector[index])
+    # for index in range(len(intensity_response_perturbed_vector)):
+    #     intensity_response_perturbed_vector[index] /= np.max(intensity_response_unperturbed_vector[index]).value
     return intensity_response_perturbed_vector
 
 
@@ -95,7 +95,7 @@ def get_input_complex_amplitude_vector(time: astropy.units.Quantity,
         time)
     input_complex_amplitude_vector = np.zeros((observatory.beam_combination_scheme.number_of_inputs,
                                                grid_size, grid_size),
-                                              dtype=complex)
+                                              dtype=complex) * observatory.instrument_parameters.aperture_radius.unit
 
     for index_input in range(observatory.beam_combination_scheme.number_of_inputs):
         input_complex_amplitude_vector[index_input] = observatory.instrument_parameters.aperture_radius * np.exp(
@@ -113,9 +113,9 @@ def get_perturbation_matrix(observatory: Observatory) -> np.ndarray:
     diagonal_of_matrix = []
     for index in range(observatory.beam_combination_scheme.number_of_inputs):
         diagonal_of_matrix.append(
-            np.random.uniform(0.4, 0.6) * np.exp(1j * np.random.uniform(-0.00000000000001, 0.00000000000001)))
+            np.random.uniform(0.8, 0.9) * np.exp(1j * np.random.uniform(-0.00000000000001, 0.00000000000001)))
 
     perturbation_matrix = np.diag(diagonal_of_matrix)
-    # perturbation_matrix = np.diag([1, 1, 1, 1])
+    perturbation_matrix = np.diag([1, 1, 1, 1])
 
     return perturbation_matrix

lifesim2/core/simulation.py
@@ -183,7 +183,7 @@ class Simulation():
                     for index_response, differential_intensity_response in enumerate(differential_intensity_responses):
                         self.output.photon_rate_time_series[source.name][wavelength][index_response][index_time] = \
                             (np.sum(differential_intensity_response * source.flux[
-                                index_wavelength] * source.shape_map)).value
+                                index_wavelength] * source.shape_map * wavelength))
 
                         if self.animator and (
                                 source.name == self.animator.source_name and
@@ -200,8 +200,8 @@ class Simulation():
                 source_name: str,
                 wavelength: astropy.units.Quantity,
                 differential_intensity_response_index: int,
-                image_vmin: float = -0.5,
-                image_vmax: float = 0.5,
+                image_vmin: float = -1,
+                image_vmax: float = 1,
                 photon_rate_limits: float = 0.1,
                 collector_position_limits: float = 50):
         """Initiate the animator object and set its attributes accordingly.

lifesim2/io/simulation_output.py
@@ -2,6 +2,7 @@ from typing import Tuple
 
 import astropy.units
 import numpy as np
+from astropy import units as u
 
 from lifesim2.util.grid import get_index_of_closest_value
 
@@ -27,10 +28,11 @@ class SimulationOutput():
         self.wavelength_bin_centers = wavelength_bin_centers
         self.photon_rate_time_series = dict((sources[key].name, dict(
             (wavelength_bin_center,
-             np.zeros((number_of_differential_intensity_responses, number_of_time_steps), dtype=float)) for
+             np.zeros((number_of_differential_intensity_responses, number_of_time_steps), dtype=float) * u.ph / u.s) for
             wavelength_bin_center in wavelength_bin_centers)) for key in sources.keys())
         self.photon_rate_time_series_total = dict((wavelength_bin_center, np.zeros(
-            (number_of_differential_intensity_responses, number_of_time_steps), dtype=float)) for wavelength_bin_center
+            (number_of_differential_intensity_responses, number_of_time_steps), dtype=float) * u.ph / u.s) for
+                                                  wavelength_bin_center
                                                   in wavelength_bin_centers)
 
     def _calculate_total_photon_rate_time_series(self):

lifesim2/util/animation.py
@@ -147,6 +147,7 @@ class Animator():
         axins.set_xticklabels([])
         axins.set_yticklabels([])
         cb.ax.tick_params(labelsize=6)
+        cb.set_label('Diff. Intensity Response (m$^2$)')
         mark_inset(ax1, axins, loc1=4, loc2=3, fc="none", ec="black")
 
         return image_intensity_response, image2
@@ -167,7 +168,7 @@ class Animator():
 
         ax2.set_title(f'Photon Rate', fontsize=10)
         ax2.set_xlabel(f'Time (h)', fontsize=8)
-        ax2.set_ylabel(f'Photon Rate (a.u.)', fontsize=8)
+        ax2.set_ylabel('Photon Rate (photons / s)', fontsize=8)
         ax2.set_xlim(0, len(time_range))
         ax2.set_ylim(-self.photon_rate_limits, self.photon_rate_limits)
         ax2.set_xticks(ticks=np.linspace(0, len(time_range), len(labels)), labels=labels)
@@ -191,8 +192,8 @@ class Animator():
 
         :param differential_intensity_responses: The differential intensity responses
         """
-        self.images[1].set_data(differential_intensity_responses[self.differential_intensity_response_index])
-        self.images[2].set_data(differential_intensity_responses[self.differential_intensity_response_index])
+        self.images[1].set_data(differential_intensity_responses[self.differential_intensity_response_index].value)
+        self.images[2].set_data(differential_intensity_responses[self.differential_intensity_response_index].value)
 
     def update_photon_rate(self, output: SimulationOutput, index_time: int):
         """Update the photon rate frame.
@@ -204,5 +205,5 @@ class Animator():
         self.time_index_list.append(index_time)
         self.photon_rate_list.append(
             output.photon_rate_time_series[self.source_name][self.closest_wavelength][
-                self.differential_intensity_response_index][index_time])
+                self.differential_intensity_response_index][index_time].value)
         self.images[3].set_data(self.time_index_list, self.photon_rate_list)