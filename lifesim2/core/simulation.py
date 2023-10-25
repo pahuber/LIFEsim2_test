Fix flux calculation

lifesim2/core/simulation.py
@@ -186,7 +186,9 @@ class Simulation():
                     for index_response, differential_intensity_response in enumerate(differential_intensity_responses):
                         self.output.photon_rate_time_series[source.name][wavelength][index_response][index_time] = \
                             (np.sum(differential_intensity_response * source.flux[
-                                index_wavelength] * source.shape_map * wavelength))
+                                index_wavelength] * source.shape_map *
+                                    self.observation.observatory.instrument_parameters.wavelength_bin_widths[
+                                        index_wavelength]))
 
                         if self.animator and (
                                 source.name == self.animator.source_name and