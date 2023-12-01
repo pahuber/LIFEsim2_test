0bbc2d699d5c4d4ff4b28dc840072b01af7e9dfc
pahuber <philhuber@live.com>
2023-12-01 18:17:34 +0100

    Update example

--- examples/single_observation_planetary_system/main.py
+++ examples/single_observation_planetary_system/main.py
@@ -3,21 +3,24 @@ from pathlib import Path
 from matplotlib import pyplot as plt
 
 from sygn.core.modules.fits_reader_module import FITSReaderModule
-from sygn.core.modules.mlm_extraction_module import MLMExtractionModule
 from sygn.core.pipeline import Pipeline
 from sygn.util.helpers import FITSDataType
 
 # Create pipeline
 pipeline = Pipeline()
 
+###############################################################
+# Generate synthetic measurements
+###############################################################
+
 # # Load configurations
 # module = ConfigLoaderModule(path_to_config_file=Path(r'config.yaml'))
 # pipeline.add_module(module)
-
+#
 # # Load target
 # module = TargetLoaderModule(path_to_context_file=Path(r'planetary_system.yaml'))
 # pipeline.add_module(module)
-
+#
 # # Make animation
 # modules = AnimatorModule(output_path='.',
 #                         source_name='Earth',
@@ -28,39 +31,60 @@ pipeline = Pipeline()
 #                         image_vmin=-10,
 #                         image_vmax=10)
 # pipelines.add_module(modules)
-
+#
 # # Generate synthetic measurements
 # module = DataGeneratorModule()
 # pipeline.add_module(module)
-
+#
 # # Write synthetic measurements to FITS file
 # module = FITSWriterModule(output_path=Path('.'), data_type=FITSDataType.SyntheticMeasurement)
 # pipeline.add_module(module)
+
+###############################################################
+# Load synthetic measurements
+###############################################################
 
 # Read synthetic measurement
 module = FITSReaderModule(input_path=Path('data_20231201_134346.934838.fits'),
                           data_type=FITSDataType.SyntheticMeasurement)
 pipeline.add_module(module)
 
+###############################################################
+# Generate templates
+###############################################################
+
 # # Generate Templates
 # module = TemplateGeneratorModule()
 # pipeline.add_module(module)
-
+#
 # # Write templates to FITS file
 # module = FITSWriterModule(output_path=Path('./templates'), data_type=FITSDataType.Template)
 # pipeline.add_module(module)
+
+
+###############################################################
+# Load templates
+###############################################################
 
 # Read templates
 module = FITSReaderModule(input_path=Path('templates'), data_type=FITSDataType.Template)
 pipeline.add_module(module)
 
-# Extract flux and position using maximum likelihood method
-module = MLMExtractionModule()
-pipeline.add_module(module)
+###############################################################
+# Process data
+###############################################################
 
+# # Extract flux and position using maximum likelihood method
+# module = MLMExtractionModule()
+# pipeline.add_module(module)
+#
 # # Fit input spectrum to extracted spectrum
 # module = SpectrumFittingModule()
 # pipeline.add_module(module)
+
+###############################################################
+# Run pipeline and analyse data
+###############################################################
 
 # Run pipeline
 pipeline.run()
